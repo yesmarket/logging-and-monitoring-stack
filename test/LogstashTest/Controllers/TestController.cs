f07e7ffc0127987e4d918d8126890f1290bec666
Ryan Bartsch <Ryan.Bartsch@certegy.com.au>
2018-11-23 17:22:11 +1030

    first commit

---
+++ test/LogstashTest/Controllers/TestController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Prometheus;
+using Serilog;
+using Serilog.Context;
+using Serilog.Formatting.Compact;
+using Serilog.Sinks.Elasticsearch;
+
+namespace LogstashTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        public const string FlatFormat = "{Timestamp:HH:mm:ss} [{Level:u3}] {Message:lj}{NewLine}{Properties}{NewLine}";
+
+        private static readonly string Host = Environment.MachineName;
+        private static readonly Counter Counter = Metrics.CreateCounter("Logs", "Count", "Host", "Type", "Direct");
+
+        // GET api/values
+        [HttpGet]
+        [Route("d")]
+        public ActionResult<string> Default()
+        {
+            return "working";
+        }
+
+        // GET api/values
+        //[HttpGet]
+        //[Route("es")]
+        //public ActionResult ElasticsearchDirect()
+        //{
+        //    Log(lc => lc.WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri("http://elasticsearch:9200")) { IndexFormat = "elasticsearch-direct" }), "Logging directly to Elasticsearch");
+        //    Counter.Labels(Host, "Elasticsearch", true.ToString()).Inc();
+        //    return Ok();
+        //}
+
+        [HttpGet]
+        [Route("ls1")]
+        public ActionResult LogstashDirect()
+        {
+            Log(lc => lc.WriteTo.LogstashHttp("http://logstash:5043"), "Logging directly to Logstash");
+            Counter.Labels(Host, "Logstash", true.ToString()).Inc();
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("ls2")]
+        public ActionResult LogstashViaDockerProvider()
+        {
+            Log(lc => lc.WriteTo.Console(new CompactJsonFormatter()), "Logging to Logstash via docker provider");
+            Counter.Labels(Host, "Logstash", false.ToString()).Inc();
+            return Ok();
+        }
+
+        private static void Log(Func<LoggerConfiguration, LoggerConfiguration> func, string message)
+        {
+            var loggerCoonfiguration = new LoggerConfiguration().Enrich.FromLogContext();
+            using (var log = func.Invoke(loggerCoonfiguration).CreateLogger())
+            {
+                using (LogContext.PushProperty("conversationId", Guid.NewGuid()))
+                {
+                    log.Information(message);
+                    log.Warning(message);
+                    log.Error(message);
+                }
+            }
+        }
+    }
+}
---
+++ test/LogstashTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Serilog;
+
+namespace LogstashTest
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseUrls("http://*:5045")
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .UseSerilog();
+    }
+}
